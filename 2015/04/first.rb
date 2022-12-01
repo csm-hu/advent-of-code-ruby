5fbfd8baf2d1c6d10f908eb7f5b056f707f9cfca
Csomay Mihály <git@csm.hu>
2022-12-01 15:34:10 +0100

    Finished 2015/04.

---
+++ 2015/04/first.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+
+secret = ARGV.first
+num = 0
+
+num += 1 until Digest::MD5.hexdigest(secret + num.to_s).start_with? "00000"
+
+puts "Lowest number to produce the required hash: #{num}"
+puts "Hash: #{Digest::MD5.hexdigest(secret + num.to_s)}"
