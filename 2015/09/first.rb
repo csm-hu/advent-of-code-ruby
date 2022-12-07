e69ee0f566fc3f3edb56579bcab2e071a583c92f
Csomay Mihály <git@csm.hu>
2022-12-07 12:12:06 +0100

    Finished 2015/09.

---
+++ 2015/09/first.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+distances = {}
+
+ARGF.each_line(chomp: true).map(&:split).each do |x, _, y, _, d|
+  distances[[x, y].sort] = d.to_i
+end
+
+results = distances.keys.flatten.uniq.permutation.map do |route|
+  route.each_cons(2).reduce(0) do |s, x|
+    s + distances[x.sort]
+  end
+end
+
+puts "Distance of the shortest route: #{results.min}"
